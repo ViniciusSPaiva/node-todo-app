c5cb8e5068359e66e0e6c42cf5fee731ad24265a
Vinicius Paiva <vinicius.silva.paiva@gmail.com>
2018-02-12 16:55:49 -0200

    add update script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -10,6 +10,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
   console.log('Connected to MongoDB server');
 
+
+  //deleteOne
+  // db.collection('Todos').deleteMany({
+  //   text: 'Something'
+  // }).then((result) => {
+  //   // console.log(JSON.stringify(result, undefined, 2));
+  //   console.log(result);
+  //   db.close();
+  // }, (err) => {
+  //   console.log('Unable to delete todos', err);
+  // });
+
   db.collection('Todos').findOneAndDelete({
     text: 'Something'
   }).then((result) => {
