123966489b4382682ec432298292a05bf69be006
Vinicius Paiva <vinicius.silva.paiva@gmail.com>
2018-02-13 23:14:07 -0200

    add delete todos by id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://master:master@ds133557.mlab.com:33557/todoapp'  
+  mlab: 'mongodb://master:master@ds133557.mlab.com:33557/todoapp'
 };
-mongoose.connect(db.mlab || db.localhost);
+if(process.env.PORT){
+  mongoose.connect(db.mlab);
+}else{
+  mongoose.connect(db.localhost);
+}
+
 
 module.exports = {mongoose};
